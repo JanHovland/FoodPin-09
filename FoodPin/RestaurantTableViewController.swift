e933e3f84ac580d2226d699f35012a568fb6bfec
Jan Hovland <janhovland@jans-imac.local>
2018-08-24 11:35:18 +0200

    Lagt inn "Service message" og avkrysning

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -55,14 +55,35 @@ class RestaurantTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Create an option menu as an action sheet
         let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
- 
+        
         // Add actions to the menu
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-    
+        
+        let callActionHandler = { (action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, this call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default , handler: callActionHandler)
+        
+        // Check in action
+        
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: { (action: UIAlertAction!)  -> Void in
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+        })
+        
         optionMenu.addAction(cancelAction)
- 
+        optionMenu.addAction(callAction)
+        optionMenu.addAction(checkInAction)
+        
+        
         // Display the menu
         present(optionMenu, animated: true, completion: nil)
  
+        // Deselect thr row
+        tableView.deselectRow(at: indexPath, animated: false)
+        
     }
 }
